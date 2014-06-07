8eef39e141a248a895279fe0b3cf2242988e708b
chino1992 <rolivermurillo@gmail.com>
2014-06-07 09:53:46 -0600

    Creación del paquete de Visual Studio

---
+++ Aplicación Visual/nutriApp/nutriApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nutriApp.Startup))]
+namespace nutriApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
